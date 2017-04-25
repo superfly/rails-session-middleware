37b7b59273d8def69ff3a1f357415f810698eeb9
goodroot <kellen@goodroot.ca>
2017-04-24 14:07:11 -1000

    Makes title not weird gibberish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
-  
+
   # Returns a full title with defaults.
   def full_title(page_title = '')
-    base_title = "Blergabelbebebe"
+    base_title = "Superfly Demo"
     if page_title.empty?
       base_title
     else
@@ -10,4 +10,3 @@ module ApplicationHelper
     end
   end
 end
-
