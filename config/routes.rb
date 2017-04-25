37e9ad74a68c6a2de74dc17bcac58641fd29f977
goodroot <kellen@goodroot.ca>
2017-04-25 10:24:34 -1000

    Scoping

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+
 
   get 'users/new'
 
   root 'static_pages#home'
-  get '/signup',    to: 'users#new'
-  post '/signup',   to: 'users#create'
-  get '/login',     to: 'sessions#new'
-  post '/login',    to: 'sessions#create'
-  delete 'logout',  to: 'sessions#destroy'
+  scope '/app' do
+    get 'sessions/new'
+    get '/signup',    to: 'users#new'
+    post '/signup',   to: 'users#create'
+    get '/login',     to: 'sessions#new'
+    post '/login',    to: 'sessions#create'
+    delete 'logout',  to: 'sessions#destroy'
+  end
   resources :users
 
 end
