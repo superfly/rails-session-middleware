f93e95e1195bbabe957abdbcbd5208236baa5d08
goodroot <root@goodroot.ca>
2017-07-25 13:06:45 -0700

    Asset-y things

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.assets.prefix = "/app/assets/"
+     config.assets.prefix = "/app/assets/"
     # config.relative_url_root = "/app"
   end
 end
